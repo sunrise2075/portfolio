55a9679cfba5de900830d3e24fe3f5f36436dace
sunrise2075 <sunrise2075@163.com>
2017-03-17 12:11:29 +0800

    add image optimization workflow and readme file

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
 	gulpPlumber = require('gulp-plumber'),
 	connect = require('gulp-connect'),
-	csslint = require('gulp-csslint');;
+	csslint = require('gulp-csslint'),
+    image = require('gulp-image');
 
 gulp.task('connect', function() {
   connect.server({
@@ -29,6 +30,21 @@ gulp.task('styles', function(){
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('build/css/'))
 		.pipe(connect.reload());
+});
+
+gulp.task('optimize-image', function () {
+    gulp.src('src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            jpegoptim: true,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10
+        })).pipe(gulp.dest('build/images'));
 });
 
 gulp.task('watch',function(){
